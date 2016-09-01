9618be36d7b1e95bf2ea3b3fe47694b1d802b2be
MatrixMike <matroxmike@icloud.com>
2016-09-01 23:30:52 +1000

    printing own created function using :: notation

--- test2.hs
+++ test2.hs
@@ -11,6 +11,7 @@ main = do
 --   myVariable :: Int
 --    myVariable = 3
     let inc x = x + 1
+    putStrLn ( "a show line " ++ show (inc 3))
     let sq x = x * x
     let sqinc x = inc x * inc x
     print myVariable
@@ -18,15 +19,18 @@ main = do
     print ans
     printNumbers
     putStrLn (show (degrees 20))
+    putStrLn (show (inc 10))
     --putStrLn (show myMap 
     
  --   print sqinc 4
  --   putStrLn ans
 printNumbers = do
     putStrLn (show (3+4))
-    let inc x = x + 1
-    let sq x = x * x
-    let sqinc x = inc x * inc x
+--    let inc x = x + 1
+--    let sq x = x * x
+--    let sqinc x = inc x * inc x
+    putStrLn ( show ( myMap inc [2,3,4]))
+    putStrLn ( show ( myMap sqinc [2,3,4]))
     putStrLn (show (sqinc 4))
     
 {-| Converts degrees into the standard angle measurement (radians). -}
@@ -35,8 +39,19 @@ degrees n = n * pi / 180
 myMap :: (a -> b ) -> [a] -> [b]
 myMap f [] = []
 myMap f (x:xs) = f x : myMap f xs
+
 myVariable :: Int
 myVariable = 3
+
+inc :: Int -> Int
+inc z = z + 1
+
+sq :: Int -> Int
+sq p = p * p
+
+sqinc :: Int -> Int
+sqinc x = inc x * inc x
+
     
 
 
