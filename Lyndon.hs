3b7a0a3e7955a81fa5e1fd30a157d3abebf35456
MikeH <matroxmike@icloud.com>
2017-02-25 11:31:10 +1100

    lists

--- Lyndon.hs
+++ Lyndon.hs
@@ -1,2 +1,16 @@
 --
-main = print "Hello World"
+--main = print "Hello World"
+main = 
+--let sq x = x * x
+
+	print  (myAdd 3 4 )
+--	print myMult 2 3 4
+-- print sq 7
+
+
+myAdd x y = x + y
+myTuple = (1,"mjh")
+myMult x y z = x*y*z
+list1 = [1,2,3]
+list2 = 1 : 2 : []
+list3 = "first" : "second" : []
