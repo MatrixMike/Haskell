e88162d73f5225c1ee1226bcbcc89c448c33120a
MikeH <matroxmike@icloud.com>
2017-02-25 12:12:22 +1100

    using lists A

--- Lyndon.hs
+++ Lyndon.hs
@@ -4,6 +4,7 @@ main =
 --let sq x = x * x
 
 	print  (myAdd 3 4 )
+--print  f  [2,3,4]   
 --	print myMult 2 3 4
 -- print sq 7
 
@@ -14,3 +15,10 @@ myMult x y z = x*y*z
 list1 = [1,2,3]
 list2 = 1 : 2 : []
 list3 = "first" : "second" : []
+
+f (x:xs) = x
+myLength [] = 0
+mylength (x:xs) = 1 + mylength xs
+
+
+
