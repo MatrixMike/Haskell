0aff55d411017e021f5b58611c257b7942d9ea13
MikeH <matroxmike@icloud.com>
2017-02-25 13:23:10 +1100

    types

--- Lyndon.hs
+++ Lyndon.hs
@@ -3,7 +3,7 @@
 main = 
 --let sq x = x * x
 
-	print  (myAdd 3 4 )
+   print  (myAdd 3 4 )
 --print  f  [2,3,4]   
 --	print myMult 2 3 4
 -- print sq 7
@@ -23,5 +23,18 @@ mylength (x:xs) = 1 + mylength xs
 myFilter f []     = []
 myFilter f (x:xs) = if f x then x : myFilter f xs
                            else     myFilter f xs
+                           
+myTuple1 :: (String, Int)
+myTuple1 = ("The meaning of life", 42)
+
+myTuple2 :: (Int, String, Int, String)
+myTuple2 = (3,"asd",6,"iuiu")
+
+
+myFloat :: (Float)
+myFloat = 1.1
+
+-- type String = [Char]
+
 
 
