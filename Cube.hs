01081cc84cd0794aa944896358aa172f7bc25cb4
Mike Hewitt <matroxmike@icloud.com>
2017-03-07 00:24:33 +1100

    minor

--- Cube.hs
+++ Cube.hs
@@ -1,21 +1,21 @@
-    module Geometry.Cube  
+module Geometry.Cube  
     ( volume  
     , area  
     ) where  
       
-    import qualified Geometry.Cuboid as Cuboid  
+import qualified Geometry.Cuboid as Cuboid  
 -- main = do 
     --putStrLn "Hello"
     
     
          
-    volume :: Float -> Float  
-    volume side = Cuboid.volume side side side  
+volume :: Float -> Float  
+volume side = Cuboid.volume side side side  
       
-    area :: Float -> Float  
-    area side = Cuboid.area side side side  
+area :: Float -> Float  
+area side = Cuboid.area side side side  
     
---main = do
+main = do Cuboid.area (2.0 3.0 4.0)
     --putStrLn ( show (zip [1,2,3] ["a","b","f"]))
 --putStrLn (7)
 --Cuboid.area (2.0 3.0 4.0)
