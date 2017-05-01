62121fbeb3b4adc297cd33d5a5327b556519c427
Mike Hewitt <matroxmike@icloud.com>
2017-05-02 08:59:06 +1000

    after hindent

--- backwards.hs
+++ backwards.hs
@@ -1,11 +1,11 @@
 -- usage is ...
-   main = do
-      line <- getLine
-      if null line
-        then return ()
-        else do
-          putStrLn $ reverseWords line
-          main
-    
-    reverseWords :: String -> String
-    reverseWords = unwords . map reverse . words
+main = do
+  line <- getLine
+  if null line
+    then return ()
+    else do
+      putStrLn $ reverseWords line
+      main
+
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
