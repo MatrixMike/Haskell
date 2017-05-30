296df402bf9af31e301c832eeef2e8006c969ed3
MikeH <matroxmike@icloud.com>
2017-05-30 17:07:02 +1000

    changed spelling

--- learnyouahaskell/output8.hs
+++ learnyouahaskell/output8.hs
@@ -1,9 +1,9 @@
     import Control.Monad  
       
     main = do   
-        colors <- forM [1,2,3,4] (\a -> do  
-            putStrLn $ "Which color do you associate with the number " ++ show a ++ "?"  
-            color <- getLine  
-            return color)  
-        putStrLn "The colors that you associate with 1, 2, 3 and 4 are: "  
-        mapM putStrLn colors  
+        colours <- forM [1,2,3,4] (\a -> do  
+            putStrLn $ "Which colour do you associate with the number " ++ show a ++ "?"  
+            colour <- getLine  
+            return colour)  
+        putStrLn "The colours that you associate with 1, 2, 3 and 4 are: "  
+        mapM putStrLn colours  
