85b45b598d1cd5423ac1e7ce68da6f2a863fae6f
Mike J Hewitt <mikeh@electroteach.com>
2017-07-25 06:45:51 +1000

    ctrl-d for end-of-data

--- learnyouahaskell/output9.hs
+++ learnyouahaskell/output9.hs
@@ -1,6 +1,9 @@
     import Control.Monad  
     import Data.Char  
-      
+{-
+when testing this - reading data from terminal 
+use ctrl-D as end-of-data and notice 'clean response'
+-}
     main = forever $ do  
         putStr "Give me some input: "  
         l <- getLine  
