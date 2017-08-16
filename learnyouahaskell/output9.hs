e9ba1ff1aa8616c13e7d7bb90513a66a35b697bf
Mike J Hewitt <mikeh@electroteach.com>
2017-08-16 19:15:24 +1000

    revisited tests2

--- learnyouahaskell/output9.hs
+++ learnyouahaskell/output9.hs
@@ -3,6 +3,7 @@
 {-
 when testing this - reading data from terminal 
 use ctrl-D as end-of-data and notice 'clean response'
+16.08.2017 19:14:50
 -}
     main = forever $ do  
         putStr "Give me some input: "  
