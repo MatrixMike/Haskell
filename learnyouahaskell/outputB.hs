271f631d2bc03c441a46f03f2e7353ddcd50b9e3
Mike J Hewitt <mikeh@electroteach.com>
2017-08-16 22:16:05 +1000

    lists experiments3

--- learnyouahaskell/outputB.hs
+++ learnyouahaskell/outputB.hs
@@ -5,5 +5,9 @@ main = do
     print (last ["one", "two","three","four"])
     print (tail ["one", "two","three","four"])
     print (init ["one", "two","three","four"])
+    print (length ["one", "two","three","four"])
+    print (null ["one", "two","three","four"])
+--    print (uncons ["one", "two","three","four"])
+    print ( ["one", "two","three","four"] ++ ["x","y"])
     
     
