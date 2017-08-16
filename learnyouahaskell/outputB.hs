a20dafa3de753bcf16c6c32479aebdc4f0853e4e
Mike J Hewitt <mikeh@electroteach.com>
2017-08-16 21:59:24 +1000

    lists experiments1

---
+++ learnyouahaskell/outputB.hs
@@ -0,0 +1,9 @@
+main = do
+--  print (map (++ "*") (map (++ "!") ["hey", "ho", "woo"]))
+    print  ["one", "two","three","four"]
+    print (head ["one", "two","three","four"])
+    print (last ["one", "two","three","four"])
+    print (tail ["one", "two","three","four"])
+    print (init ["one", "two","three","four"])
+    
+    
