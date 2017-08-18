643cffe27c5854d0e27e2a944475470e9e0085ed
Mike Hewitt <matroxmike@icloud.com>
2017-08-18 15:22:22 +1000

    added URL

--- logic1.hs
+++ logic1.hs
@@ -1,5 +1,6 @@
 -- 18.08.2017 05:27:56
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+{-https://stackoverflow.com/questions/27507393/using-bitwise-and-operation-in-haskell-}
 import Data.Bits
 x :: Bool
 x = True
