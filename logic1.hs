422cc465848b75e64b4c4406ae0765f196e95892
Mike Hewitt <matroxmike@icloud.com>
2017-08-18 14:24:26 +1000

    bitwise logic

--- logic1.hs
+++ logic1.hs
@@ -1,11 +1,23 @@
 -- 18.08.2017 05:27:56
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+import Data.Bits
+x :: Bool
 x = True
 
+y :: Bool
 y = False
 
+t :: Int    {- without this get some interesting & helpful results from ghc-}
+t = (.&.) 6 5
+
+
+main :: IO ()
 main = do
   print x
   print y
   print (x && y)
   print (x || y)
+  print (not (x || y))
+  print  t
+  
+
