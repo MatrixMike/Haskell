cb48d3c164a9f7e925cccf01f45e7f27ad943f8d
Mike Hewitt <matroxmike@icloud.com>
2017-10-11 00:28:23 +1100

    first partially useful cut but algorithm wrong

---
+++ SinR.hs
@@ -0,0 +1,15 @@
+--radius :: Floating -> Floating -> Floating
+-- 11.10.2017 00:24:13
+x::Int
+x=3
+
+y::Int
+y=4
+
+sroot :: Int -> Int -> Float
+sroot x y = sqrt(fromIntegral x * fromIntegral x) + sqrt(fromIntegral y * fromIntegral y)
+
+
+main = do
+    print $  sroot 3 4
+    
