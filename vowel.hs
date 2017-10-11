b88b37583e4bf47990f20c50235827d0435661aa
Mike Hewitt <matroxmike@icloud.com>
2017-10-11 20:20:52 +1100

    guards

---
+++ vowel.hs
@@ -0,0 +1,30 @@
+-- 08.10.2017 17:30:47
+module Main where
+
+--import Text.CaseConversion
+import Data.Char
+import Data.List
+
+toCharacterCase ::   Char -> [String] -> String
+toCharacterCase c cs = intercalate [c] (map (map toLower) cs)
+
+{-
+convert given string to lower case and test each character for vowels
+   for each character :
+   if vowel then add nothing to o/p string 
+    else add character 
+-}
+remVowels :: Char -> Char
+remVowels  c ='a'
+--         | c ='e'
+--         | c ='i'
+--         | c ='o'
+--         | c ='u'  
+         | otherwise  = 'z'
+         
+    
+
+main :: IO ()
+main = do
+    putStrLn remVowels "Hewitt"
+    
