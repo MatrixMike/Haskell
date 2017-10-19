af129bc2541951d5458f8c8c67139314a541cbfc
Mike J Hewitt <mikeh@electroteach.com>
2017-10-20 03:57:24 +1100

    Float stuff3

--- doubleTest.hs
+++ doubleTest.hs
@@ -4,22 +4,18 @@
 {-
 https://stackoverflow.com/questions/3968265/integer-to-float
 -}
-
-main :: IO()
+main :: IO ()
 main = do
-    print $ posToXY 3.0 1.2
-    print $ posToXYf  3.0 1.2
-    
+  print $ posToXY 3.0 1.2
+  print $ posToXYf 3.0 1.2
 
 -- main = print 3.0 / 2.0 :: Fractional a => a -> a -> a
 posToXY :: Float -> Float -> Integer
 posToXY a b = do
-        let y = a / b
-        round y
-        
+  let y = a / b
+  round y
+
 posToXYf :: Float -> Float -> Float
 posToXYf a b = do
-        let y = a / b
-        y
-        
-        
+  let y = a / b
+  y
