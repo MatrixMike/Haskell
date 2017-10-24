032a2f0795e1c43d90421a9332e44c7391f5d7cb
Mike Hewitt <matroxmike@icloud.com>
2017-10-24 22:45:34 +1100

    bike gear ratios

---
+++ bikegears1.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
+--import Control.Applicative
+{-
+try with and without the import Control.Applicative
+https://hackage.haskell.org/package/base-4.10.0.0/docs/Control-Applicative.html
+http://learnyouahaskell.com/making-our-own-types-and-typeclasses#the-functor-typeclass
+not sure what is going on here (yet)
+-}
+main :: IO ()
+main = do
+--    print $ (+) <$> (* 10) <$> [1, 2, 3] <*> [4, 5, 6]  -- was (* 10)
+--    print $ (+) <$> (/ 10) <$> [1, 2, 3] <*> [4, 5, 6]  --
+--    print $ (+) <$>  [1, 2, 3] <*> [4, 5, 6]  --
+    print $ (/)  <$> [22.0, 32.0, 42.0] <*> [26, 23, 21, 19, 17, 15, 13, 12]
+--    print $ fmap (* 0.5) [1..6]
+
+{-
+see clojure/bikegears
+need to restrict decimal points, sort the output, organise into columns
+-}
