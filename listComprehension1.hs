cbd2c914b4f8606efb71fe0f08a4cc50091ad051
Mike J Hewitt <mikeh@electroteach.com>
2017-10-27 15:15:34 +1100

    filtering vowels from strings2

--- listComprehension1.hs
+++ listComprehension1.hs
@@ -4,25 +4,30 @@ import Data.Char (toUpper)
 https://wiki.haskell.org/List_comprehension
 -}
 remVowels :: Char -> Char
-remVowels c 
-           | c =='a' = 'x'
-           | c =='e' = 'x'
-           | c =='i' = 'x'
-           | c =='o' = 'x'
-           | c =='u' = 'x'  
-           | otherwise  = 'z'
-           
+remVowels c
+  | c == 'a' = x
+  | c == 'e' = x
+  | c == 'i' = x
+  | c == 'o' = x
+  | c == 'u' = x
+  | otherwise = 'z'
+  where  x = 'x'
+
 s :: String
 s = "Hello"
 
+b = map toUpper [remVowels c | c <- ['a' .. 'z']]
+--print b
+
+
 main :: IO ()
-main = do
+main
 --  let a = [toUpper c | c <- "some text"]
-   let s1 = ['a'..'w']
-   let a = [toUpper c | c <- [' '..'~']]
-   print a
-   let b = [remVowels c | c <- ['a'..'z']]
-   print b
+ = do
+  let s1 = ['a' .. 'w']
+  let a = [toUpper c | c <- [' ' .. '~']]
+  print a
+  print b
 
 {-
 "HELLO"
