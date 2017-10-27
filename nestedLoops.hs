3e0c9688a7eda4060a1b9955aaff91610994d16c
MatrixMike <matroxmike@icloud.com>
2017-10-27 11:46:02 +1100

    format of lists2

--- nestedLoops.hs
+++ nestedLoops.hs
@@ -16,7 +16,11 @@ main = do
     print [x * 10 + y | x <- [1 .. 3], y <- [4 .. 6]]
     print 5
 --    print [fx / fy | fx <- [22.0 32.0 42.0], fy <- [26 23 21 19 17 15 13 12]]
-    print [fx  / fy   | fx <- [22.0, 32.0, 42.0], fy <- [26.0, 23.0]]
+    let bg = [fx  / fy   | fx <- [22.0, 32.0, 42.0], fy <- [26, 23, 21, 19, 17, 15, 13, 12]]
+    print [fx  / fy   | fx <- [22.0, 32.0, 42.0], fy <- [26, 23, 21, 19, 17, 15, 13, 12]]
+    print "@@@@@"
+    print bg
+    
 {-
 https://stackoverflow.com/questions/16904427/nested-loop-equivalent
 -}
