0891bff6e9335d5a993afdc272b8e598314a8cb3
Mike Hewitt <matroxmike@icloud.com>
2017-10-29 17:56:48 +1100

    testing

--- anonFunc.hs
+++ anonFunc.hs
@@ -1,10 +1,18 @@
-
-
-main = print $ (\x -> x + 1) 4
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
 
 -- (\x y -> x + y) 3 5
+addOneList' :: Num b => [b] -> [b]
 addOneList' lst = map (\x -> x + 1) lst
 
+myList = [3,4,6,7]
+
+main :: IO()
+main = do
+    print $ (\x -> x + 1) 4
+    print $ addOneList' myList
+    
+
+-- for x11-example consider a list (of offsets) that we call a function drawLine on
 -- http://dobegin.com/lambda-functions-everywhere/
 -- https://www.haskell.org/tutorial/functions.html
 
