41dc3613776c2b6fe064f7d085c59b79cb6544a8
Phyllis <matroxmike@icloud.com>
2017-10-29 17:46:01 +1100

    experiments

---
+++ anonFunc.hs
@@ -0,0 +1,10 @@
+
+
+main = print $ (\x -> x + 1) 4
+
+-- (\x y -> x + y) 3 5
+addOneList' lst = map (\x -> x + 1) lst
+
+-- http://dobegin.com/lambda-functions-everywhere/
+-- https://www.haskell.org/tutorial/functions.html
+
