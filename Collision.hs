bef2a3954ea9e1e9f527553772ee34b17e539df9
Mike J Hewitt <mikeh@electroteach.com>
2017-10-30 14:40:36 +1100

    missing module - benefit of testing on another PC

---
+++ Collision.hs
@@ -0,0 +1,23 @@
+module Collision
+   (
+   collidesWith
+   )
+   where
+
+
+collidesWith :: (Double, Double) -> (Double, Double) -> Bool
+collidesWith (x,y) (x',y') = (x'-x)^2 + (y-y')^2 < collisionRadius^2
+  where
+    collisionRadius = 4
+{-
+x  = collidesWith(3.0, 4.9) (3.1, 4.8)
+    
+main :: IO()
+main = do
+    print 6
+    print   $  collidesWith(3.0, 4.9) (3.1, 4.8)
+    print   $  collidesWith(3.0, 4.9) (3.1, 9.8)
+    print x
+  -}
+    
+    
