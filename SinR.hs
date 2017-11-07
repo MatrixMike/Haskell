692119cd6268a236f2775152ba16064a58ed2ecd
MatrixMike <matroxmike@icloud.com>
2017-11-07 19:38:52 +1100

    list comprehension on own function

--- SinR.hs
+++ SinR.hs
@@ -1,10 +1,11 @@
 --radius :: Floating -> Floating -> Floating
 -- 11.10.2017 00:24:13
-module SinR 
+{-module SinR 
     (
     sroot
     )
     where
+-}
    
 x::Int
 x=3
@@ -16,8 +17,11 @@ sroot :: Int -> Int -> Float
 sroot x y = sqrt(fromIntegral x * fromIntegral x + fromIntegral y * fromIntegral y)
 
 
-{-
+
 main = do
     print $  sroot 3 4
--}
+    print [y+x^2 | x <- [1,2..7]]
+    print [sroot x y |y <- [1,2..7]]  -- list comprehension on one argument 
+    
+
    
