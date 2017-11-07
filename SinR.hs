9c7ab121908719a5b43d8c33452d4327ee60790c
MatrixMike <matroxmike@icloud.com>
2017-11-07 20:26:12 +1100

    multiple list comprehensions

--- SinR.hs
+++ SinR.hs
@@ -1,3 +1,4 @@
+--     
 --radius :: Floating -> Floating -> Floating
 -- 11.10.2017 00:24:13
 {-module SinR 
@@ -16,12 +17,24 @@ y=4
 sroot :: Int -> Int -> Float
 sroot x y = sqrt(fromIntegral x * fromIntegral x + fromIntegral y * fromIntegral y)
 
+bg :: [Double]
+bg = (/)  <$> [22.0, 32.0, 42.0] <*> [26, 23, 21, 19, 17, 15, 13, 12]
+
 
 
 main = do
     print $  sroot 3 4
     print [y+x^2 | x <- [1,2..7]]
-    print [sroot x y |y <- [1,2..7]]  -- list comprehension on one argument 
+    print $ sroot x y 
+--    print  sroot (x, y)
+--    print [sroot x  y |y <- [1,2..7]]    -- good
+    print [sroot x y | x <- [1,2.. 6], y <- [1,2..7]]
+      -- list comprehension on one argument  
+    --map sroot (x,y) [x | x <- [1,2.. 6], y |y <- [1,2..7]]
+    --  Use ParallelListComp
+ --   print bg  --good
+    
+    
     
 
    
