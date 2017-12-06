c127e25f0ba5402dd36adb11b3acc80ba5ad8348
Mike J Hewitt <mikeh@electroteach.com>
2017-12-06 17:38:03 +1100

    parallel resonance 2

--- Electronics/resonanceP.hs
+++ Electronics/resonanceP.hs
@@ -1,17 +1,17 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
 {-
 https://www.allaboutcircuits.com/textbook/alternating-current/chpt-6/parallel-tank-circuit-resonance/
 -}
+capacitor :: Double
+capacitor = 10e-6 -- 10uF
 
-capacitor :: Double 
-capacitor = 10e-6   -- 10uF
+inductor :: Double
+inductor = 100e-3 -- 100mH
 
-inductor  :: Double
-inductor = 100e-3   -- 100mH
+frequency :: Double
+frequency = 1 / (2 * pi * sqrt (capacitor * inductor))
 
-frequency :: Double 
-frequency  = 1 / ( 2 * pi * sqrt ( capacitor * inductor))
-
-main :: IO()
-main = do 
-    print frequency
+main :: IO ()
+main = do
+  print frequency
