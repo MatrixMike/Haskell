77ceb53e2fad3048e48acd6785bf526091cd62b2
Mike J Hewitt <mikeh@electroteach.com>
2017-12-15 21:05:47 +1100

    split the list

--- xmas.hs
+++ xmas.hs
@@ -2,8 +2,11 @@
 main :: IO()
 main = do 
     let s = [ x * (13-x)   |  x <- [12,11..1]]
---    print [ x * (13-x)   |  x <- [12,11..1]]
-    print s 
+    let t = [ x * (13-x)   |  x <- [12,11..7]]
+
+    print s
+    print t
     print $ sum s 
+    print $ 2 * sum t 
 --    putStrLn s
 --    print [x [x<- [12,11..1]]]
