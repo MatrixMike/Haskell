cf07f7f597064fcbf0be640591ea4e6bacff48cd
Mike J Hewitt <mikeh@electroteach.com>
2017-12-16 12:07:30 +1100

    move display centre

--- Pascal.hs
+++ Pascal.hs
@@ -10,10 +10,10 @@ pascal :: [[Int]]
 pascal = iterate (\row -> zipWith (+) ([0] ++ row) (row ++ [0])) [1]
 
 main :: IO ()
-main = mapM_ putStrLn $ ((flip center 40 . unwords) . map show) <$> take 16 pascal
+main = mapM_ putStrLn $ ((flip center 70 . unwords) . map show) <$> take 16 pascal
 
 -- equivalently, more elaborated
-main' = display $ prettify $ stringify $ take 10 pascal
+main' = display $ prettify $ stringify $ take 16 pascal
     where stringify = (fmap . fmap) show
           prettify = fmap (flip center 40 . unwords)
           display = mapM_ putStrLn
