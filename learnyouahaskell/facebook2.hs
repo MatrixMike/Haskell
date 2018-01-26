3238f419d1c79cbe12a8aee6678ae783f1d6b7e6
Mike J Hewitt <mikeh@electroteach.com>
2018-01-27 09:42:36 +1100

    mini-mod2

--- learnyouahaskell/facebook2.hs
+++ learnyouahaskell/facebook2.hs
@@ -3,15 +3,22 @@
 
 main :: IO()
 main = do
-    print (head ["one", "two","three","four"])
+{-    print (head ["one", "two","three","four"])
     print (last ["one", "two","three","four"])
     print (tail ["one", "two","three","four"])
     print (init ["one", "two","three","four"])
-    -- note output characters or string : elements or list
-    let word = "one"
-    print (head "one")
-    print (last "one")
-    print (tail "one")
-    print (init "one")
+-}
+  -- note output characters or string : elements or list
+    let word = "Linux"    -- try with own choice of word
     
+    print (head word)
+    print (tail word)
     
+    print (init word)
+    print (last word)
+
+    print (init (tail (word)))
+      
+-- works for lists with 2 or more elements
+-- easy fix for lists with 1 element (but do later)
+   
