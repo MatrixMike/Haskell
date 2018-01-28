c0247548a83a3c4d7117e674071d0b06532e7aa1
Mike J Hewitt <mikeh@electroteach.com>
2018-01-29 09:43:49 +1100

    update thinking2

--- Vowel2.hs
+++ Vowel2.hs
@@ -1,11 +1,11 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
 -- 08.10.2017 17:30:47
 -- 11.10.2017 21:36:05
 -- http://learnyouahaskell.com/syntax-in-functions
 {-module Main where
 -}
-
 {-module Vowel2 
     (
     remVowels,
@@ -18,7 +18,7 @@
 import Data.Char
 import Data.List
 
-toCharacterCase ::   Char -> [String] -> String
+toCharacterCase :: Char -> [String] -> String
 toCharacterCase c cs = intercalate [c] (map (map toLower) cs)
 
 {-
@@ -28,35 +28,33 @@ convert given string to lower case and test each character for vowels
     else add character 
 -}
 remVowels :: Char -> Char
-remVowels c 
-           | c =='a' = 'x'
-           | c =='e' = 'x'
-           | c =='i' = 'x'
-           | c =='o' = 'x'
-           | c =='u' = 'x'  
-           | otherwise  = 'z'
-         
+remVowels c
+  | c == 'a' = 'x'
+  | c == 'e' = 'x'
+  | c == 'i' = 'x'
+  | c == 'o' = 'x'
+  | c == 'u' = 'x'
+  | otherwise = 'z'
+
 remVowelsL :: Char -> Bool
-remVowelsL c 
-           | c =='a' = True
-           | c =='e' = True
-           | c =='i' = True
-           | c =='o' = True
-           | c =='u' = True
-           | otherwise  = False
- 
+remVowelsL c
+  | c == 'a' = True
+  | c == 'e' = True
+  | c == 'i' = True
+  | c == 'o' = True
+  | c == 'u' = True
+  | otherwise = False
+
 -- https://stackoverflow.com/questions/38858857/getting-vowels-from-a-string-and-printing-them-in-haskell 
 -- https://stackoverflow.com/questions/46364784/haskell-checking-the-vowels-in-a-string?rq=1
 -- https://en.wikipedia.org/wiki/Vowel_length
-
 isVowel :: Char -> Bool
-isVowel x = x `elem` "aeiou"     
+isVowel x = x `elem` "aeiou"
+
 vowel :: String -> String
 vowel = filter isVowel
 
 main :: IO ()
 main = do
-    print $  remVowels 'a'
-    print  $ vowel "The cat sat on the mat"
-    
-    
+  print $ remVowels 'a'
+  print $ vowel "The cat sat on the mat"
