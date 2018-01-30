fd3dd2b5b547205fae3ef8daf633e3a189c99325
Mike Hewitt <matroxmike@icloud.com>
2018-01-30 23:50:26 +1100

    takes multiple words on a line

--- backwards/singleWord.hs
+++ backwards/singleWord.hs
@@ -14,3 +14,6 @@ h2 = head word
 main :: IO ()
 main = do
     print $ reverse (middle word)
+    putStrLn "starting -> please supply input...on which to perform letter shuffling "
+    interact (unlines . map middle . lines)
+    putStrLn "finishing..."
