749b87a06cc6255013983f0c6eea7d97d2e9447a
Mike J Hewitt <mikeh@electroteach.com>
2018-02-02 08:37:14 +1100

    tip from LYAH higher-order-functions

--- backwards/Middlew.hs
+++ backwards/Middlew.hs
@@ -3,9 +3,13 @@ module Middlew
   ) where
 middle :: String -> String
 middle = unwords . map middle1 . words  
+{-
+for . see function composition 
+--http://learnyouahaskell.com/higher-order-functions
+-}
 
 middle1 :: String -> String
-middle1   =   middlep    --add reverse somehow -- "abcdef" --
+middle1 s   =  reverse (middlep s)  --add reverse somehow -- "abcdef" --
 
 
 -- http://learnyouahaskell.com/higher-order-functions
@@ -17,5 +21,7 @@ middlep    s
     | length s < 3  = reverse s
     | length s < 2 = s
     | length s > 2 = last s :middlep (reverse (init ( tail s ))) ++ [head s]
+--    | length s > 2 = last s:middlep (reverse (init . tail) s ) ++ [head s]
 
-
+--negabs 
+--negabs =  (negate . abs) [5,-3,-6,7,-3,2,-19,24] 
