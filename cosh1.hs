57633d07db5e3c9de552c5e90747254d00746e48
Mike J Hewitt <mikeh@electroteach.com>
2018-02-23 23:25:39 +1100

    formula may be wrong but concept is OK

--- cosh1.hs
+++ cosh1.hs
@@ -6,4 +6,8 @@ catenary a x = a * cosh ( a / x )
 
 main :: IO()
 main = do
+    let a = 0.2
     print [cosh x | x <- [0.1, 0.2.. 2.0]]
+    putStrLn "xxx"
+    print [catenary a x | x <- [0.1, 0.2.. 2.0]]
+
