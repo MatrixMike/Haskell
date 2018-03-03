9dec1f29f581898d7f42a705b27c2e60b6ba2415
Mike Hewitt <matroxmike@icloud.com>
2018-03-03 17:25:13 +1100

    after lint

--- Bezier.hs
+++ Bezier.hs
@@ -7,10 +7,10 @@ import Data.List
 import Graphics.Gloss
 import Graphics.Gloss.Data.Vector
 
-{- Bezier curve approximation 
+{- Bezier curve approximation
    See: http://en.wikipedia.org/wiki/B%C3%A9zier_curve
    Compile: ghc -O2 Bezier.hs
-   Run: ./Bezier 
+   Run: ./Bezier
  -}
 type Bezier = [Point] -- A linear, quadratic, cubic or higher order Bézier line segment
                         -- E.g. [P1, P2] (linear) or [P1, P2, P3, P4] (cubic)
@@ -23,8 +23,8 @@ subV :: Vector -> Vector -> Vector
 subV (a, b) (c, d) = (a - c, b - d)
 
 -- generate pairs from list [e1, e2, ... ] as [(e1,e2), (e2,e3), ... (e(n-1),en)]
-make_pairs :: [a] -> [(a, a)]
-make_pairs es =
+makePairs :: [a] -> [(a, a)]
+makePairs es =
   let es' = take (length es - 1) es
       es'' = drop 1 es
   in zip es' es''
@@ -36,18 +36,18 @@ listify (a, b) = [a, b]
 bezier :: Float -> Bezier -> Vector
 bezier u (p1:p2:[]) = p1 + mulSV u (p2 `subV` p1) -- linear
 bezier u ps =
-  let pairs = map (listify) $ make_pairs ps -- quadratic or higher
-  in bezier u $ (map (bezier u)) $ pairs
+  let pairs = map listify $ makePairs ps -- quadratic or higher
+  in bezier u $ map (bezier u) pairs
 
 -- Draw Bézier as picture
-draw_bezier :: Bezier -> Picture
-draw_bezier ps =
+drawBezier :: Bezier -> Picture
+drawBezier ps =
   let p1 = head ps   -- list actions head
       pn = last ps   -- list actions last
-      nsteps = (magV $ pn `subV` p1) / 5 -- ~ 5 pixel steps 
+      nsteps = magV ( pn `subV` p1) / 5 -- ~ 5 pixel steps
       steps = [0,(min 0.5 (1 / nsteps)) .. 1]
-      curve = Color green $ Line $ map (\u -> bezier u ps) steps
-      linear = Color orange $ Line [p1, pn]  --elements from the list 
+      curve = Color green $ Line $ map (`bezier` ps) steps   -- was (\u -> bezier u ps) , now
+      linear = Color orange $ Line [p1, pn]  --elements from the list
       --  try another colour to mid-point between the ends
       outer = Color (greyN 0.75) $ Line ps
   in pictures [curve, linear, outer]  -- as defined in the let above
@@ -66,5 +66,5 @@ main = do
   display
     (InWindow "Bézier" (600, 600) (100, 100))
     white -- background
-    (draw_bezier b1 )
+    (drawBezier b1 )
 
