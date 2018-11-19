9a6c418e795228e192e0b142b976ea4d92c62c12
Mike H <matroxmike@icloud.com>
2018-11-20 01:04:32 +1100

    added exp

--- cosh2.hs
+++ cosh2.hs
@@ -12,3 +12,5 @@ main = do
   print [cosh x | x <- [0.1,0.2 .. 2.0]]
   putStrLn "xxx"
   print [catenary a x | x <- [-0.7,-0.65 .. 0.7]]
+  print $ exp 1     -- added just to test expression to get 2.718...  Naperian Logs 
+  print $ (1 - 1/(exp 1))
