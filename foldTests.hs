5a6949cfa7f27d56945d6fe3d57c7575067e04aa
Mike H <matroxmike@icloud.com>
2019-10-16 20:33:57 +1100

    after hindent on TECRA

--- foldTests.hs
+++ foldTests.hs
@@ -1,22 +1,26 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
-import Data.Char
+
 import Control.Applicative
+import Data.Char
+
 -- http://zvon.org/other/haskell/Outputprelude/foldr_f.html
 list :: [Integer]
 --list = [1, 2, 3, 4, 5]
-list = [1..5]
-x = [1..5]
-y = ['a'..'e']
+list = [1 .. 5]
+
+x = [1 .. 5]
+
+y = ['a' .. 'e']
+
 --let x = 41
-
 -- foldr , foldl could be read as fold from RIGHT, fold from LEFT
-
 main :: IO ()
 main = do
-    print $ foldr (-) 0 [2,3,4] 
-    print $ foldl (-) 0 [2,3,4]
+  print $ foldr (-) 0 [3, 4]
+  print $ foldr (-) 0 [2, 3, 4]
+  print $ foldr (-) 2 [3, 4]
 --    print $ foldl (-) 0 [2,3,4]
 --    print $ foldr (-) 
-    print "xx"
-    print $ foldr1 (\x y -> (x+y)/2) [12,4,10,6]
-    
+  print "xx"
+  print $ foldr1 (\x y -> (x + y) / 2) [12, 4, 10, 6]
+  print $ foldr1 (\x y -> (x + y) / 2) [4, 10, 6]
--- listComprehension1.hs
+++ listComprehension1.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
 import Data.Char (toUpper)
 
 {-
@@ -12,28 +13,28 @@ remVowels c
   | c == 'o' = c
   | c == 'u' = c
   | otherwise = 'z'
-  where  x = 'x'
+  where
+    x = 'x'
 
 s :: String
 s = "Hello"
 
-b:: [Char]
-b = {-map toUpper-} [remVowels c | c <- ['a' .. 'z']]
+b :: [Char]
+b = [remVowels c | c <- ['a' .. 'z']] {-map toUpper-}
+
 --print b
-
-
 main :: IO ()
 main
 --  let a = [toUpper c | c <- "some text"]
  = do
   let s1 = ['a' .. 'w']
-  let a = [{-toUpper-} c | c <- [' ' .. '~']]
+  let a = [c | c <- [' ' .. '~']] {-toUpper-}
   print a
   print b
-  print [ x*y | x <- [2,5,10], y <- [8,10,11], x*y > 50]  
+  print [x * y | x <- [2, 5, 10], y <- [8, 10, 11], x * y > 50]
   --http://learnyouahaskell.com/starting-out#im-a-list-comprehension
-  print [ x*y*z | x <- [2,5,10], y <- [8,10,11], x*y > 50, z <- [2]]  
-
+  print "zzz"
+  print [x * y * z | x <- [2, 5, 10], y <- [8, 10, 11], x * y > 50, z <- [2]]
 {-
 "HELLO"
 -}
