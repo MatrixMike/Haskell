4f2dd7dc144b042791b91732646129f9f7d78b15
Mike H <matroxmike@icloud.com>
2019-10-16 20:37:57 +1100

    after hindent on TECRA

--- cosh/cosh1.hs
+++ cosh/cosh1.hs
@@ -1,13 +1,12 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
 -- 04.03.2018 13:05:00
 catenary :: Floating a => a -> a -> a
-catenary a x = a * cosh ( x / a )
+catenary a x = a * cosh (x / a)
 
-
-main :: IO()
+main :: IO ()
 main = do
-    let a = 0.1
-    print [cosh x | x <- [0.1, 0.2.. 2.0]]
-    putStrLn "xxx"
-    print [catenary a x | x <- [-0.7,-0.6.. 0.7]]
-
+  let a = 0.1
+  print [cosh x | x <- [0.1,0.2 .. 2.0]]
+  putStrLn "xxx"
+  print [catenary a x | x <- [-0.7,-0.6 .. 0.7]]
