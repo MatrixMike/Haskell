42c4a70e7900c7904484d2286d84a8f3d21b2c20
Mike H <matroxmike@icloud.com>
2019-12-03 21:58:41 +1100

    transfer working to another PC

--- DateStuff.hs
+++ DateStuff.hs
@@ -19,12 +19,12 @@ month=12
 months=["JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER"]
 mnth=months!!(month-1)
 monthDate = getDaysInMonth year month
-startAt=snd(monthDate)
-totalDays = fst(monthDate)
+startAt=snd monthDate 
+totalDays = fst monthDate 
 printLine = "\n"++concat ["+----\t" | r <- [1..7]]
 check x | x>totalDays=""
         | x<=totalDays=show x
-genDays n m="\n "++concat[check(x)++"\t|"|x<-[n..m]]
+genDays n m="\n "++concat[check x ++"\t|"|x<-[n..m]]
 genD 6=""
 genD sP=genDays ((sP-1)*7+1) (sP*7) ++genD (sP+1)
 generateCalendar=printLine++"\n "++
